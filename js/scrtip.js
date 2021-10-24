a71c3296e0cfaab7b8ad6ce691e3887bc9022cc6
Leonel Carvajal <liosoft777@gmail.com>
2021-10-23 18:17:27 -0600

    Terminando diseño en Escritorio , comenzando con JS

--- js/scrtip.js
+++ js/scrtip.js
@@ -5,6 +5,50 @@ const arrowPrevious = document.querySelector('.arrow__left');
 const arrowNext = document.querySelector('.arrow__right');
 const cart = document.getElementById('cart');
 const basketModal = document.querySelector('.basket__modal');
+const boxCart = document.querySelector('.box__cart');
+const imgLightBox = document.querySelector('.lightbox__img')
+const imagesThumbnail = document.querySelectorAll('.img__thumbanil')
+
+let index = 0
+const arrayImages = [
+  'image-product-1.jpg',
+	'image-product-2.jpg',
+	'image-product-3.jpg',
+	'image-product-4.jpg',
+];
+
+const initialImage = ()=>{	
+	imgLightBox.src =`images/${arrayImages[index]}`
+}
+
+
+
+const ChangeImage = (arrow) => {
+  if (arrow === 'back') {
+    index--;
+    if (index < 0) {
+      index = arrayImages.length - 1;
+    }
+    //miniatures[index].classList.add("miniatures__img--active");
+    initialImage();
+  } else if (arrow === 'forward') {
+    index++;
+    if (index >= arrayImages.length) {
+      index = 0;
+    }
+    //miniatures[index].classList.add("miniatures__img--active");
+    initialImage();
+    
+  }
+};
+const activeImg = (e)=>{
+  imagesThumbnail.forEach(img=>{
+    if(img.classList.contains('img__thumbnail--active')){
+      img.classList.remove('img__thumbnail--active')
+    }
+  })
+  e.target.classList.add('img__thumbnail--active')
+}
 
 menuImg.addEventListener('click', () => {
 	if (!menu.classList.contains('menu--active')) {
@@ -18,12 +62,25 @@ menuClose.addEventListener('click', () => {
 });
 
 arrowPrevious.addEventListener('click', (e) => {
-	console.log(e.target);
+  ChangeImage('back')
 });
+
 arrowNext.addEventListener('click', (e) => {
-	console.log(e.target);
+	ChangeImage('forward')
 });
 
 cart.addEventListener('click', () => {
 	basketModal.classList.toggle('basket__modal--active');
 });
+
+boxCart.addEventListener('click', () => {
+	basketModal.classList.toggle('basket__modal--active');
+});
+
+imagesThumbnail.forEach(img=>{
+  img.addEventListener('click',(e)=>{
+    activeImg(e)
+  })
+})
+
+window.addEventListener('DOMContentLoaded',initialImage)
